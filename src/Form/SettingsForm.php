60b07bfea055d04dad263fec16cac577b8034d83
Beverly Lanning <beverly_lanning@reyrey.com>
2018-04-26 15:00:27 -0400

    renamed the controller. added js file that resets the toolbar by removing the sticky-default classes and set to override core's BodyVisualView.js and BodyVisualView.es6.js file.

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -9,7 +9,7 @@ namespace Drupal\sticky_toolbar\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Form\FormState;
-use Drupal\sticky_toolbar\Controller\StickySetting;
+use Drupal\sticky_toolbar\Controller\StickySettingController;
 
 class SettingsForm extends FormBase {
 	/**
@@ -19,12 +19,12 @@ class SettingsForm extends FormBase {
 		return 'sticky_toolbar_settings_form';
 	}
 
-  /**
+    /**
 	 * {@inheridoc}
 	 */
 	public function buildForm(array $form, FormStateInterface $form_state) {
 		// get user's existing settings
-		$currentUser = new StickySetting();
+		$currentUser = new StickySettingController();
 		$setting = $currentUser->getSetting();
 
 		// fields available on admin settings screen
@@ -41,12 +41,15 @@ class SettingsForm extends FormBase {
 
 		return $form;
 
-		// @todo: Find more elegant way to create a single StickySetting object for both functions.
+		// @todo: Find more elegant way to create a single StickySettingController object for both functions.
 	}
 
+	/**
+	 * {@inheridoc}
+	 */
 	public function submitForm(array &$form, FormStateInterface $form_state) {
 		$sticky = $form_state->getValue('is_sticky');
-		$currentUser = new StickySetting();
+		$currentUser = new StickySettingController();
 		if (is_integer($sticky)) {
 			$currentUser->setSetting($sticky);
 		}
